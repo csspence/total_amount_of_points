198ba974ad9d64e3d0aa93770f61f587ff358163
Crew Spence <csspence123@gmail.com>
2020-03-13 07:13:42 -0500

    Wrote working solution for total_amount_of_points.

--- total_amount_of_points.js
+++ total_amount_of_points.js
@@ -1,9 +1,11 @@
 /*
-Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the 
+collection.
 
 For example: ["3:1", "2:2", "0:1", ...]
 
-Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for 
+each match:
 
 if x>y - 3 points
 if x<y - 0 point
@@ -15,6 +17,19 @@ there are 10 matches in the championship
 0 <= y <= 4
 */
 
-function points(games) {
-  // your code here
+const points = (games) => {
+  let totalPoints = 0;
+  for(let i = 0; i < games.length; i++) {
+    if(parseInt(games[i][0]) > parseInt(games[i][2])) {
+      totalPoints += 3;
+    }
+    if(parseInt(games[i][0]) < parseInt(games[i][2])) {
+      continue;
+    }
+    if(parseInt(games[i][0]) === parseInt(games[i][2])) {
+      totalPoints += 1;
+    }
+  }
+
+  return totalPoints;
 }
